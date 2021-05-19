7a5931c3e259cd2618864fe15a97c09726b637bd
9MB_macbookpro <why9901679@gmail.com>
2021-05-19 16:17:44 +0800

    Fetched JobList Completed
    Please add job application page, with validation on whether member account activated

--- screens/JobListScreen.js
+++ screens/JobListScreen.js
@@ -10,21 +10,93 @@ import {
   Dimensions
 } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
+import firestore from "@react-native-firebase/firestore";
 
 const locationOptions = [
   "全選",
-  "九龍東區(KEC)",
-  "九龍中區(KCC)",
-  "九龍西區(KWC)",
-  "新界東區(NTEC)",
-  "新界西區(NTWC)",
-  "港島東區(HKEC)",
-  "港島西區(HKWC)",
+  "九龍東(KEC)",
+  "九龍中(KCC)",
+  "九龍西(KWC)",
+  "新界東(NTEC)",
+  "新界西(NTWC)",
+  "港島東(HKEC)",
+  "港島西(HKWC)",
 ];
 
 export default function JobListScreen() {
   const [filterLocation, setFilterLocation] = React.useState("");
   const [isExpandingPicker, switchIsExpandingPicker] = React.useState(false);
+  const [fetchJobArray, setFetchJobArray] = React.useState([]);
+  const [filteredJobArray, setFilteredJobArray] = React.useState([]);
+
+  React.useEffect(() => {
+    const now = new Date();
+    async function fetchJobsFromDB() {
+      let tmpJobArray = [];
+      firestore().collection("jobs")
+        .where("recruiting", "==", true)
+        .where("startTime", ">", now)
+        .get()
+        .then(querySnapshot => {
+          querySnapshot.forEach(doc => {
+            tmpJobArray.push(doc.data())
+          })
+          setFetchJobArray([...tmpJobArray]);
+          setFilteredJobArray([...tmpJobArray]);
+          console.log("Fetched", fetchJobArray)
+        })
+        .catch(e => {
+          console.log("Error at fetching JobList From DB", e);
+        })
+    }
+    fetchJobsFromDB();
+  }, [])
+
+  function filterJobByLocation(region) {
+    var tmpFetchedJobArray = fetchJobArray;
+    setFilterLocation(region);
+    switch (region) {
+      case "全選":
+        setFilteredJobArray([...tmpFetchedJobArray]);
+        break;
+      case "九龍東(KEC)":
+        setFilteredJobArray([...tmpFetchedJobArray.filter(job => job.institutionRegion == "九龍東(KEC)")]);
+        break;
+      case "九龍中(KCC)":
+        setFilteredJobArray([...tmpFetchedJobArray.filter(job => job.institutionRegion == "九龍中(KCC)")]);
+        break;
+      case "九龍西(KWC)":
+        setFilteredJobArray([...tmpFetchedJobArray.filter(job => job.institutionRegion == "九龍西(KWC)")]);
+        break;
+      case "新界西(NTWC)":
+        setFilteredJobArray([...tmpFetchedJobArray.filter(job => job.institutionRegion == "新界西(NTWC)")]);
+        break;
+      case "新界東(NTEC)":
+        setFilteredJobArray([...tmpFetchedJobArray.filter(job => job.institutionRegion == "新界東(NTEC)")]);
+        break;
+      case "港島東(HKEC)":
+        setFilteredJobArray([...tmpFetchedJobArray.filter(job => job.institutionRegion == "港島東(HKEC)")]);
+        break;
+      case "港島西(HKWC)":
+        setFilteredJobArray([...tmpFetchedJobArray.filter(job => job.institutionRegion == "港島西(HKWC)")]);
+        break;
+    }
+  }
+
+  renderItem = ({ item }) => {
+    return (
+      <TouchableOpacity style={styles.jobRowContainer}>
+        <View>
+          <Text style={styles.jobCompanyTitle}>{item.institutionName}</Text>
+          <Text style={styles.jobTime}>{new Date(item.startTime.seconds * 1000).getMonth() + 1}月{new Date(item.startTime.seconds * 1000).getDate()}日{new Date(item.startTime.seconds * 1000).getHours() < 10 ? '0' + new Date(item.startTime.seconds * 1000).getHours() : new Date(item.startTime.seconds * 1000).getHours()}:{new Date(item.startTime.seconds * 1000).getMinutes() < 10 ? '0' + new Date(item.startTime.seconds * 1000).getMinutes() : new Date(item.startTime.seconds * 1000).getMinutes()}-{new Date(item.endTime.seconds * 1000).getHours() < 10 ? '0' + new Date(item.endTime.seconds * 1000).getHours() : new Date(item.endTime.seconds * 1000).getHours()}:{new Date(item.endTime.seconds * 1000).getMinutes() < 10 ? '0' + new Date(item.endTime.seconds * 1000).getMinutes() : new Date(item.endTime.seconds * 1000).getMinutes()}</Text>
+        </View>
+        <View style={styles.regionBox}>
+          <Text style={styles.regionText}>{item.institutionRegion.slice(0, 3)}</Text>
+        </View>
+      </TouchableOpacity>
+    )
+  }
+
   return (
     <SafeAreaView style={styles.backgroundContainer}>
       <TextInput
@@ -44,7 +116,7 @@ export default function JobListScreen() {
           <ScrollView>
             {locationOptions.map((region) => {
               return (
-                <TouchableOpacity style={styles.optionContainer} key={region.id} onPress={()=>{setFilterLocation(region)}}>
+                <TouchableOpacity style={styles.optionContainer} key={region.id} onPress={() => { filterJobByLocation(region) }}>
                   <Text style={styles.optionText}>{region}</Text>
                 </TouchableOpacity>
               );
@@ -53,13 +125,19 @@ export default function JobListScreen() {
         </View>
       ) : null}
       <View style={styles.flatListContainer}>
-      <FlatList />
+        <FlatList
+          renderItem={renderItem}
+          data={filteredJobArray}
+          ListEmptyComponent={
+            <Text style={styles.emptyListText}>您選擇的地區未有工作招聘</Text>
+          }
+        />
       </View>
     </SafeAreaView>
   );
 }
 
-const {height} = Dimensions.get("window");
+const { height, width } = Dimensions.get("window");
 const styles = StyleSheet.create({
   backgroundContainer: {
     backgroundColor: "#ffcc00",
@@ -75,9 +153,9 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     minHeight: 40,
     marginTop: 20,
-    color:"white",
-    fontSize:18,
-    textAlign:"center",
+    color: "white",
+    fontSize: 18,
+    textAlign: "center",
   },
   pickerContainer: {
     height: "25%",
@@ -95,12 +173,47 @@ const styles = StyleSheet.create({
   optionText: {
     fontFamily: "SF-Pro-Text-Regular",
   },
-  flatListContainer:{
-    width:"95%",
-    height:"88%",
-    alignSelf:"center",
-    backgroundColor:"white",
-    margin:20,
-    borderRadius:10
+  flatListContainer: {
+    width: "95%",
+    height: "88%",
+    alignSelf: "center",
+    backgroundColor: "white",
+    margin: 20,
+    borderRadius: 10
+  },
+  emptyListText: {
+    fontFamily: "SF-Pro-Rounded-Ultralight",
+    fontSize: 20,
+    alignSelf: "center",
+    marginTop: 20
+  },
+  jobRowContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    borderBottomWidth: 0.3,
+    padding: 15
+  },
+  jobCompanyTitle: {
+    fontFamily: "SF-Pro-Text-Bold",
+    fontSize: 20
+  },
+  jobTime: {
+    fontFamily: "SF-Pro-Text-Regular",
+    marginTop: 5
+  },
+  regionBox: {
+    backgroundColor: "#3CAEA3",
+    marginLeft: "auto",
+    height: "50%",
+    width: "15%",
+    alignSelf: "center",
+    borderRadius: 26,
+    textAlign: "center",
+    justifyContent: "center"
+  },
+  regionText: {
+    fontFamily: "SF-Pro-Text-Regular",
+    color: "white",
+    alignSelf: "center"
   }
 });
